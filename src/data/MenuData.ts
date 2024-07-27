6fe00e5554082baeb5a99b9076e7f66ce5b57753
Amir-Thp <code2time@gmail.com>
2024-07-27 12:11:32 +0330

    create Data as Local

---
+++ src/data/MenuData.ts
@@ -0,0 +1,67 @@
+import Cappuccione from '../assets/Menu/Cappuccione.jpg'
+import Americano from '../assets/Menu/Americano.jpg'
+import Macchiato from '../assets/Menu/Macchiato.jpg'
+import Americanpancakes from '../assets/Menu/American Pancakes.jpg'
+import TacoSoup from '../assets/Menu/Taco Soup.jpg'
+import Ristretto from '../assets/Menu/Ristretto.jpg'
+
+
+export const MenuData = [
+    {
+        id : 1,
+        title : "Cappuccione",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "breakfast",
+        price : "49",
+        img : Cappuccione
+
+    },
+    {
+        id : 2,
+        title : "Americano",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "dinner",
+        price : "120",
+        img : Americano
+
+    },
+    {
+        id : 3,
+        title : "Macchiato",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "Budget Meal",
+        price : "70",
+        img : Macchiato
+
+    }, 
+    {
+        id : 4,
+        title : "American pancakes",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "breakfast",
+        price : "38",
+        img : Americanpancakes
+
+    },
+    {
+        id : 5,
+        title : "Taco Soup",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "Lunch",
+        price : "95",
+        img : TacoSoup
+
+    },
+    {
+        id : 6,
+        title : "Ristretto",
+        description : "Usage of the Internet is becoming more common due to rapid advance.",
+        category : "Buffet",
+        price : "60",
+        img : Ristretto
+
+    },
+
+
+]
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
-// import { useState } from "react";
-
 import AboutOurStory from "../components/AboutOurStory";
 import Container from "../components/Container";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 
 function Home() {
-  console.log(typeof Header);
+
 
   return (
     <>
