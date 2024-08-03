1a1ee6c78328c62ccf162a73e4fa34aafffe79c7
Amir-Thp <code2time@gmail.com>
2024-08-03 08:37:50 +0330

    update "Reserve" component

--- src/components/Reserve.tsx
+++ src/components/Reserve.tsx
@@ -1,20 +1,70 @@
-import React from 'react'
-
 function Reserve() {
   return (
-    <div className='reserve-container w-full h-[600px] mt-52'>
-     <div className='overlay w-full h-full flex flex-col justify-center items-center sm:flex-row sm:justify-between '>
-        <div className='rederve-title text-my-white p-7 '>
-            <h1 className='poppins-7 text-xl '>Reserve Your Seats to Confirm if You Come with Your Family</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
+    <div className="reserve-container w-full h-auto my-52 ">
+      <div className="overlay w-full h-full grid grid-cols-12 gap-4 py-20 px-20 sm:px-24">
+        <div className="rederve-title text-my-white p-7  col-span-12 sm:col-span-6 text-center ">
+          <h1 className="poppins-7 text-xl  sm:w-[80%] text-justify capitalize">
+            Reserve Your Seats to Confirm if You Come with Your Family
+          </h1>
+          <p className="sm:w-[90%] md:w-[70%] text-gray-400 my-10 text-justify p-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor <span className="hidden md:block">
+            incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam. Quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea.
+            </span>
+          </p>
         </div>
-        <div className='rederve-title text-my-white p-7 '>
-            <h1>Reserve Your Seats to Confirm if You Come with Your Family</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
+        <div className="col-span-12 sm:col-span-6 md:col-span-4 h-[300px] sm:h[400px] ">
+          <div className="reserve-form-container w-full h-full border rounded bg-my-white">
+            <form>
+              <div className="w-full h-full flex flex-col">
+                <div className="w-full h-auto py-2 px-6">
+                  <input
+                    required
+                    type="text"
+                    maxLength={12}
+                    placeholder="Your Name"
+                    className="w-full h-full"
+                  />
+                </div>
+                <div className="w-full h-auto py-2 px-6">
+                  <input
+                    required
+                    type="email"
+                    placeholder="Your email address"
+                    className="w-full h-full"
+                  />
+                </div>
+                <div className="w-full h-auto py-2 px-6">
+                  <input
+                    required
+                    type="number"
+                    maxLength={12}
+                    placeholder="Your Phone Number"
+                    className="w-full h-full"
+                  />
+                </div>
+                <div className="w-full h-auto py-2 px-6">
+                  <input
+                    required
+                    type="date"
+                    
+                    className="w-full h-full"
+                  />
+                </div>
+                <div className="w-full h-auto py-2 px-6">
+                  <button className="py-2 px-3 bg-red1 outline-none  poppins-5 transition-all btn text-my-white rounded-sm mt-5">
+                    Make Reaservation
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-     </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Reserve
+export default Reserve;
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,6 @@ export default {
         },
       },
     },
-    plugins: [  require('@tailwindcss/forms')],
+    plugins: []
   },
 };
