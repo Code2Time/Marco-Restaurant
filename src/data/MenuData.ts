b651592f6505c15622716f3df16cf7014138413e
Amir-Thp <code2time@gmail.com>
2024-08-03 09:51:45 +0330

    update Data and add some Items

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -1,67 +1,181 @@
-import Cappuccione from '../assets/Menu/Cappuccione.jpg'
-import Americano from '../assets/Menu/Americano.jpg'
-import Macchiato from '../assets/Menu/Macchiato.jpg'
-import Americanpancakes from '../assets/Menu/American Pancakes.jpg'
-import TacoSoup from '../assets/Menu/Taco Soup.jpg'
-import Ristretto from '../assets/Menu/Ristretto.jpg'
-
+import Cappuccione from "../assets/Menu/Cappuccione.jpg";
+import Americano from "../assets/Menu/Americano.jpg";
+import Macchiato from "../assets/Menu/Macchiato.jpg";
+import Americanpancakes from "../assets/Menu/American Pancakes.jpg";
+import TacoSoup from "../assets/Menu/Taco Soup.jpg";
+import Ristretto from "../assets/Menu/Ristretto.jpg";
+import Bacon from "../assets/Menu/Bacon.jpg";
+import Chickenandwaffles from "../assets/Menu/Chicken and waffles.jpg";
+import Dosa from "../assets/Menu/Dosa.jpg";
+import Danishpastry from "../assets/Menu/Danish pastry.jpg";
+import Granola from "../assets/Menu/Granola.jpg";
+import Pasta from '../assets/Menu/Pasta.jpg'
+import Lomosaltado from '../assets/Menu/Lomo saltado.jpg'
+import Cabbageroll from '../assets/Menu/Cabbageroll.jpg'
+import Chickennugget from '../assets/Menu/Chicken nuggets.jpg'
+import Pudding from '../assets/Menu/pudding.jpg'
+import ChiliCheeseDogs from '../assets/Menu/Chili Cheese Dogs.jpg'
 
 export const MenuData = [
-    {
-        id : 1,
-        title : "Cappuccione",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "breakfast",
-        price : "49",
-        img : Cappuccione
-
-    },
-    {
-        id : 2,
-        title : "Americano",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "dinner",
-        price : "120",
-        img : Americano
-
-    },
-    {
-        id : 3,
-        title : "Macchiato",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "Meal",
-        price : "70",
-        img : Macchiato
-
-    }, 
-    {
-        id : 4,
-        title : "American pancakes",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "breakfast",
-        price : "38",
-        img : Americanpancakes
-
-    },
-    {
-        id : 5,
-        title : "Taco Soup",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "Lunch",
-        price : "95",
-        img : TacoSoup
-
-    },
-    {
-        id : 6,
-        title : "Ristretto",
-        description : "Usage of the Internet is becoming more common due to rapid advance.",
-        category : "Buffet",
-        price : "60",
-        img : Ristretto
-
-    },
-
-
-]
-
+  {
+    id: 1,
+    title: "Cappuccione",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "breakfast",
+    price: "49",
+    img: Cappuccione,
+  },
+  {
+    id: 2,
+    title: "Americano",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "dinner",
+    price: "120",
+    img: Americano,
+  },
+  {
+    id: 3,
+    title: "Macchiato",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "Lunch",
+    price: "70",
+    img: Macchiato,
+  },
+  {
+    id: 4,
+    title: "American pancakes",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "breakfast",
+    price: "38",
+    img: Americanpancakes,
+  },
+  {
+    id: 5,
+    title: "Taco Soup",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "Lunch",
+    price: "95",
+    img: TacoSoup,
+  },
+  {
+    id: 6,
+    title: "Ristretto",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "Buffet",
+    price: "60",
+    img: Ristretto,
+  },
+  {
+    id: 7,
+    title: "Bacon",
+    description:
+      "Bacon is a type of salt-cured pork[1] made from various cuts, typically the belly or less fatty parts of the back. ",
+    category: "breakfast",
+    price: "140",
+    img: Bacon,
+  },
+  {
+    id: 8,
+    title: "Chicken and waffles",
+    description:
+      "Chicken and waffles is an American dish combining chicken with waffles. It is part of a variety of culinary traditions",
+    category: "breakfast",
+    price: "199",
+    img: Chickenandwaffles,
+  },
+  {
+    id: 9,
+    title: "Dosa",
+    description:
+      "A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice.",
+    category: "breakfast",
+    price: "100",
+    img: Dosa,
+  },
+  {
+    id: 10,
+    title: "Danish pastry",
+    description:
+      "A Danish pastry (Danish: wienerbrø(sometimes shortened to danish, especially in American English) is a multilayered",
+    category: "breakfast",
+    price: "79",
+    img: Danishpastry,
+  },
+  {
+    id: 11,
+    title: "Granola",
+    description:
+      "A Danish pastry (Danish: wienerbrø(sometimes shortened to danish, especially in American English) is a multilayered",
+    category: "breakfast",
+    price: "50",
+    img: Granola,
+  },
+  {
+    id: 12,
+    title: "Pasta",
+    description:
+      "A Danish pastry (Danish: wienerbrø(sometimes shortened to danish, especially in American English) is a multilayered",
+    category: "dinner",
+    price: "300",
+    img: Pasta,
+  },
+  {
+    id: 13,
+    title: "Lomo saltado",
+    description:
+      "Lomo saltado is a popular, traditional Peruvian dish, a stir fry that typically combines marinated strips of sirloin (or other beef steak) with onions, tomatoes, french fries",
+    category: "dinner",
+    price: "210",
+    img: Lomosaltado,
+  },
+  {
+    id: 14,
+    title: "Cabbage roll",
+    description:
+      "A cabbage roll is a dish consisting of cooked cabbage leaves wrapped around a variety of fillings.",
+    category: "dinner",
+    price: "190",
+    img: Cabbageroll,
+  },
+  {
+    id: 15,
+    title: "Chicken nugget",
+    description:
+      "A chicken nugget is a food product consisting of a small piece of deboned chicken meat that is breaded or battered, then deep-fried or baked.",
+    category: "dinner",
+    price: "340",
+    img: Chickennugget,
+  },{
+    id: 16,
+    title: "Pudding",
+    description:
+      "Pudding is a type of food. It can be either a dessert, served after the main Lunch",
+    category: "dinner",
+    price: "50",
+    img: Pudding,
+  },
+  {
+    id: 17,
+    title: "Chili Cheese Dogs",
+    description:
+      "These chili cheese dogs are perfect for a quick and satisfying Lunch",
+    category: "Lunch",
+    price: "95",
+    img: ChiliCheeseDogs,
+  },
+  {
+    id: 18,
+    title: "Macchiato",
+    description:
+      "Usage of the Internet is becoming more common due to rapid advance.",
+    category: "Buffet",
+    price: "70",
+    img: Macchiato,
+  },
+];
