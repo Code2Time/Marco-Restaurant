0a2ebbd31763b8e8b5db632a6881ee961e26af84
Amir-Thp <code2time@gmail.com>
2024-08-09 09:06:31 +0330

    <-

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import PageNotFound from './pages/PageNotFound'
-import Gallery from './pages/Gallery'
 import Contact from './pages/Contact'
+import Orders from './pages/Orders'
 import About from './pages/About'
 import Home from './pages/Home'
 
@@ -13,7 +13,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='*' element={<PageNotFound />} />
       <Route path='/about' element={<About />} />
-      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/orders' element={<Orders />} />
       <Route path='/contact' element={<Contact />} />
      </Routes>
   )
---
+++ src/pages/Orders.tsx
@@ -0,0 +1,9 @@
+function Orders() {
+  return (
+    <div>
+        orders page
+    </div>
+  )
+}
+
+export default Orders
