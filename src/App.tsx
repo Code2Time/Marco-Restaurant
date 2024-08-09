2948aae27810e452ef123ad9e88ebc180fb3b3a6
Amir-Thp <code2time@gmail.com>
2024-08-09 12:45:01 +0330

    Create "Scoroll-Setting" File

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import PageNotFound from './pages/PageNotFound'
-import Contact from './pages/Contact'
-import Order from './pages/Order'
-import About from './pages/About'
-import Home from './pages/Home'
-
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
+import Contact from "./pages/Contact";
+import Order from "./pages/Order";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import ScrollToTop from "./scroll-setting/ScorollSetting";
 
 function App() {
- 
-
   return (
-     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='*' element={<PageNotFound />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/orders/:id' element={<Order />} />
-      <Route path='/contact' element={<Contact />} />
-     </Routes>
-  )
+    <>
+    <ScrollToTop/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/orders/:id" element={<Order />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
